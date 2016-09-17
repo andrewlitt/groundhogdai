a48b39bd5e8ed8b0d030995711e3597bd2752277
Jake Alsemgeest <alsemgeester@gmail.com>
2016-09-17 19:18:43 -0400

    Testing jenkins.

--- src/views/Main/routes.js
+++ src/views/Main/routes.js
@@ -4,7 +4,7 @@ import {IndexRoute, Route, IndexRedirect} from 'react-router';
 import Container from './Container';
 import Search from './Search/Search';
 
-export const mainRoutes = () => {
+export const mainRoutes = () => { 
   return (
     <Route path='/' component={Container}>
       <IndexRoute component={Search}/>
