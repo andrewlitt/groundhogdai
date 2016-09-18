ca9f614320ded1ced38fd67f6f60b6663d538d2e
Andrew Litt <andrew.litt@queensu.ca>
2016-09-18 03:08:50 -0400

    Changed message and stuff.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -11,6 +11,8 @@ export class About extends React.Component {
     return (
         <div className={styles.about}>
           <h1>What is Groundhog d.AI?</h1>
+          <p>Here we have to write a little blurb about our site.</p>
+          <p>It will touch on technical details, funny shit, and other cool stuff.</p>
         </div>
     );
   }
